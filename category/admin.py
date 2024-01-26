4a9c67fba0f20a5799790ef3815822f3e2085c2a
moooooora <omarmohammed271@gmail.com>
2024-01-26 19:28:30 +0200

    make Category model and show it in admin panel

--- category/admin.py
+++ category/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
-
+from .models import Category
 # Register your models here.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['category_name','slug']
+    
+
+
+
+
+
+
+
+
+
+# class CategoryAdmin(admin.ModelAdmin):
+#     list_display = ['category_name','slug']
+
+
+# admin.site.register(Category,CategoryAdmin)
--- category/models.py
+++ category/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.text import slugify
 
+
+def image_upload(instance,file_name:str):
+    extension = file_name.split('.')[1]
+    return f'Categories/{instance.category_name}.{extension}'
 # Create your models here.
+class Category(models.Model):
+    category_name = models.CharField( max_length=250)
+    slug = models.SlugField(unique=True,blank=True,null=True)
+    image = models.ImageField( upload_to=None, height_field=None, width_field=None, max_length=None)
+
+    def save(self,*args, **kwargs):
+        self.slug = slugify(self.category_name)
+        super(Category,self).save(*args, **kwargs)
+    class Meta:
+        verbose_name = ("Category")
+        verbose_name_plural = ("Categories")
+
+    def __str__(self):
+        return self.category_name
+
+    # def get_absolute_url(self):
+    #     return reverse("Category_detail", kwargs={"pk": self.pk})
